05715681f381ee32bef3ca2b8b302a8610dcda32
sNOMORE-dev <s33015979@gmail.com>
2026-07-10 18:22:53 +0530

    Add user object with personal details

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+{
+  fullName: "Aman Khan",
+  email: "aman@example.com",
+  phone: "9999999999",
+  role: "student",
+  password: "123456",
+  points: 0,
+  level: 1,
+  savedExams: [],
+  upgraded: false
+}
